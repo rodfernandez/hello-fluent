ad19f6c6e04fabc118002fd5c15c994b12390be1
Rodrigo Fernandez <rodfernandez@users.noreply.github.com>
2024-04-04 14:59:33 -0700

    pushed first

--- src/myfunction.js
+++ src/myfunction.js
@@ -2,5 +2,8 @@ import { gs } from '@servicenow/glide'
 
 export const showStatusUpdate = function (newStatus, oldStatus) {
     gs.addInfoMessage(`Record updated. Old Status: ${oldStatus}`)
+
+    // PUSHED FIRST!
+    
     gs.addInfoMessage(`New Status: ${newStatus}`)
 }
